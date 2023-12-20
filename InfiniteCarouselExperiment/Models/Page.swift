b71e95b1942fb05e51ba9c7ef4d6d7061b19390d
mkotyak <kotyak.maria@gmail.com>
2023-12-20 12:01:32 +0300

    Implemented infinite carousel

--- InfiniteCarouselExperiment/Models/Page.swift
+++ InfiniteCarouselExperiment/Models/Page.swift
@@ -3,4 +3,16 @@ import SwiftUI
 struct Page: Identifiable, Hashable {
     var id: UUID = .init()
     let color: Color
+    
+    static var firstPage: Self {
+        .init(color: .red)
+    }
+    
+    static var secondPage: Self {
+        .init(color: .green)
+    }
+    
+    static var thirdPage: Self {
+        .init(color: .blue)
+    }
 }
--- InfiniteCarouselExperiment/Views/HomeView.swift
+++ InfiniteCarouselExperiment/Views/HomeView.swift
@@ -2,9 +2,9 @@ import SwiftUI
 
 struct HomeView: View {
     @State private var currentPage: UUID = .init()
-    @State private var listOfPages: [Page] = []
+    @State private var fakedPages: [Page] = []
 
-    @State var fakedPages: [Page] = []
+    private var defaultPages: [Page] = [.firstPage, .secondPage, .thirdPage]
 
     var body: some View {
         VStack {
@@ -16,23 +16,8 @@ struct HomeView: View {
                 return
             }
 
-            for color in [Color.yellow, Color.blue, Color.pink] {
-                listOfPages.append(.init(color: color))
-            }
-
-            fakedPages.append(contentsOf: listOfPages)
-
-            if var firstPage = listOfPages.first,
-               var lastPage = listOfPages.last
-            {
-                currentPage = firstPage.id
-
-                firstPage.id = .init()
-                lastPage.id = .init()
-
-                fakedPages.append(firstPage)
-                fakedPages.insert(lastPage, at: 0)
-            }
+            currentPage = defaultPages[0].id
+            fakedPages = [.thirdPage] + defaultPages + [.firstPage]
         }
     }
 
@@ -43,60 +28,33 @@ struct HomeView: View {
     }
 
     private var carouselView: some View {
-        GeometryReader {
-            let size = $0.size
-
-            TabView(selection: $currentPage) {
-                ForEach(fakedPages) { page in
-                    RoundedRectangle(cornerRadius: 20)
-                        .fill(page.color.gradient)
-                        .padding(.horizontal, 20)
-                        .tag(page.id)
-
-                        // Calculating Entire Page Scroll Offset
-                        .offsetX(currentPage == page.id) { rect in
-                            let minX = rect.minX
-                            let pageOffset = minX - (size.width * CGFloat(fakeIndex(of: page)))
-
-                            // Converting Page Offset into Progress
-                            let pageProgress = pageOffset / size.width
-
-                            // Infinit Carousel logic
-                            if -pageProgress < 1.0 {
-                                // Movig to the last page
-                                // Which is actually the First Duplicated
-                                if fakedPages.indices.contains(fakedPages.count - 1) {
-                                    currentPage = fakedPages[fakedPages.count - 1].id
-                                }
-                            }
-
-                            if -pageProgress > CGFloat(fakedPages.count - 1) {
-                                // Movig to the first page
-                                // Which is actually the Last Duplicated
-                                if fakedPages.indices.contains(1) {
-                                    currentPage = fakedPages[1].id
-                                }
-                            }
+        TabView(selection: $currentPage) {
+            ForEach(fakedPages) { page in
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(page.color.gradient)
+                    .padding(.horizontal, 20)
+                    .tag(page.id)
+                    .onDisappear {
+                        if currentPage == fakedPages.first?.id {
+                            currentPage = fakedPages[3].id
+                        } else if currentPage == fakedPages.last?.id {
+                            currentPage = fakedPages[1].id
                         }
-                }
+                    }
             }
-            .tabViewStyle(.page(indexDisplayMode: .never))
-            .overlay(alignment: .bottom) {
-                PageControlView(
-                    totalPages: listOfPages.count,
-                    currentPage: originalIndex(of: currentPage)
-                )
-                .padding(.bottom, 15)
-            }
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        .overlay(alignment: .bottom) {
+            PageControlView(
+                totalPages: defaultPages.count,
+                currentPage: originalIndex(of: currentPage)
+            )
+            .padding(.bottom, 16)
         }
     }
 
-    private func fakeIndex(of page: Page) -> Int {
-        fakedPages.firstIndex(of: page) ?? 0
-    }
-
     private func originalIndex(of pageID: UUID) -> Int {
-        listOfPages.firstIndex { page in
+        defaultPages.firstIndex { page in
             page.id == pageID
         } ?? 0
     }
